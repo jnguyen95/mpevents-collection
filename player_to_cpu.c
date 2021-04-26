dd169f3e55a805b59621932f27fe50d2afcb77ff
Mastertimethief <Mastertimethief@gmail.com>
2021-04-26 17:23:15 -0400

    Adding MTT files

---
+++ player_to_cpu.c
@@ -0,0 +1,61 @@
+// NAME: Change Player to CPU
+// GAMES: MP3_USA
+// EXECUTION: Direct
+
+#include "ultra64.h"
+
+struct player {
+    s8 unk0;
+    s8 cpu_difficulty;
+    s8 controller;
+    u8 character;
+    /**
+     * Miscellaneous flags.
+     * 1: Is CPU player
+     */
+    u8 flags;
+    s8 pad0[5];
+    s16 coins; // 10
+    s16 minigame_coins; // 12
+    s8 stars; // 14
+
+    u8 cur_chain_index; // 15
+    u8 cur_space_index; // 16
+    u8 next_chain_index; // 17
+    u8 next_space_index; // 18
+    u8 unk1_chain_index; // 19
+    u8 unk1_space_index; // 20
+    u8 reverse_chain_index; // 21
+    u8 reverse_space_index; // 22
+
+    u8 flags2; // 23
+    u8 items[3]; // 24
+    u8 bowser_suit_flag; // 27
+    u8 turn_color_status; // 28
+
+    s8 pad1[7]; // 29 - 35
+
+    void *obj; // 36 // struct object *
+    s16 minigame_star; // 40
+    s16 coin_star; // 42
+    s8 happening_space_count; // 44
+    s8 red_space_count;
+    s8 blue_space_count;
+    s8 chance_space_count;
+    s8 bowser_space_count; // 48
+    s8 battle_space_count;
+    s8 item_space_count;
+    s8 bank_space_count;
+    s8 game_guy_space_count; // 52
+
+    // s8 pad2[3];
+}; // sizeof == 56
+
+
+void main() {
+    struct player *player = GetPlayerStruct(GetCurrentPlayerIndex());
+
+    //Flip this bit to swap whether the player is a human or CPU
+    player->flags = player->flags ^ 1;
+    
+}
---
+++ ultra64.h
@@ -0,0 +1,16 @@
+/**************************************************************************
+THIS FILE IS NOT TO BE INCLUDED IN PARTY PLANNER
+
+This file is here so my compiler doesn't throw errors for this file
+not existing, and it's helpful to define things in here that the real
+ultra64.h would define
+ *************************************************************************/
+
+typedef signed char            s8;
+typedef unsigned char          u8;
+typedef signed short int       s16;
+typedef unsigned short int     u16;
+typedef signed int             s32;
+typedef unsigned int           u32;
+typedef float                  f32;
+typedef double                 f64;
