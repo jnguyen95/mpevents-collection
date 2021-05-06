52e4c0e5528d6969e4090b49d222f0e312bbd583
Justin Nguyen <jnnguyen50@gmail.com>
2021-05-05 19:47:37 -0500

    Barter box and Lucky Coin event added

---
+++ lucky_coin.c
@@ -0,0 +1,129 @@
+// NAME: Lucky Coin Event (MP3)
+// GAMES: MP3_USA
+// EXECUTION: Direct
+// AUTHOR: Kyon2570
+
+// BASE CODE: Give Item
+// https://github.com/PartyPlanner64/events/blob/master/C/Give%20Item%20(MP3).c
+
+#include "ultra64.h"
+
+enum item {
+    ITEM_NONE = 0xFF,
+    ITEM_MUSHROOM = 0x00,
+    ITEM_SKELETON_KEY = 0x01,
+    ITEM_POISON_MUSHROOM = 0x02,
+    ITEM_REVERSE_MUSHROOM = 0x03,
+    ITEM_CELLULAR_SHOPPER = 0x04,
+    ITEM_WARP_BLOCK = 0x05,
+    ITEM_PLUNDER_CHEST = 0x06,
+    ITEM_BOWSER_PHONE = 0x07,
+    ITEM_DUELING_GLOVE = 0x08,
+    ITEM_LUCKY_LAMP = 0x09,
+    ITEM_GOLDEN_MUSHROOM = 0x0A,
+    ITEM_BOO_BELL = 0x0B,
+    ITEM_BOO_REPELLANT = 0x0C,
+    ITEM_BOWSER_SUIT = 0x0D,
+    ITEM_MAGIC_LAMP = 0x0E,
+    ITEM_KOOPA_CARD = 0x0F,
+    ITEM_BARTER_BOX = 0x10,
+    ITEM_LUCKY_COIN = 0x11,
+    ITEM_WACKY_WATCH = 0x12
+};
+
+struct player {
+    s8 unk0;
+    s8 cpu_difficulty;
+    s8 controller;
+    u8 character;
+    /**
+     * Miscellaneous flags.
+     * 1: Is CPU player
+     */
+    u8 flags;
+    s8 pad0[5];
+    /**
+     * Current coin count.
+     */
+    s16 coins; // 10
+    /**
+     * Coins obtained during a Mini-Game.
+     */
+    s16 minigame_coins; // 12
+    s8 stars; // 14
+
+    u8 cur_chain_index; // 15
+    u8 cur_space_index; // 16
+    u8 next_chain_index; // 17
+    u8 next_space_index; // 18
+    u8 unk1_chain_index; // 19
+    u8 unk1_space_index; // 20
+    u8 reverse_chain_index; // 21
+    u8 reverse_space_index; // 22
+
+    u8 flags2; // 23
+    u8 items[3]; // 24
+    u8 bowser_suit_flag; // 27
+    u8 turn_color_status; // 28
+
+    s8 pad1[7]; // 29 - 35
+
+    void *obj; // 36 // struct object *
+    s16 minigame_star; // 40
+    s16 coin_star; // 42
+    s8 happening_space_count; // 44
+    s8 red_space_count;
+    s8 blue_space_count;
+    s8 chance_space_count;
+    s8 bowser_space_count; // 48
+    s8 battle_space_count;
+    s8 item_space_count;
+    s8 bank_space_count;
+    s8 game_guy_space_count; // 52
+
+    // s8 pad2[3];
+}; // sizeof == 56
+
+extern s16 GetCurrentPlayerIndex();
+extern struct player *GetPlayerStruct(s32 player_index);
+extern s32 PlayerHasEmptyItemSlot(s32 player_index);
+
+char *lucky_coin_msg =
+    "You find something on the ground"
+    "\x85\x85\x85" //...
+    "\xFF";
+
+char *lucky_coin_not_full_inventory_msg =
+    "It\x5Cs a Lucky Coin\xC2 It must be your lucky day\xC2"
+    "\x0A"
+    "You picked up the Lucky Coin\x85"
+    "\xFF";
+
+char *lucky_coin_full_inventory_msg =
+    "It\x5Cs a bison dollar\x85"
+    "\x0A"
+    "You leave the bison dollar on the ground\x85"
+    "\xFF";
+
+void cleanupMessage() {
+    func_800EC9DC();
+    CloseMessage();
+    func_800EC6EC();
+}
+
+void main() {
+    s32 cur_player_index = GetCurrentPlayerIndex();
+    ShowMessage(-1, lucky_coin_msg, 0, 0, 0, 0, 0);
+    cleanupMessage();
+    s32 open_index = PlayerHasEmptyItemSlot(cur_player_index);
+    if (open_index != -1) {
+        struct player *player = GetPlayerStruct(cur_player_index);
+        ShowMessage(-1, lucky_coin_not_full_inventory_msg, 0, 0, 0, 0, 0);
+        cleanupMessage();
+        player->items[open_index] = ITEM_LUCKY_COIN;
+    }
+    else {
+        ShowMessage(-1, lucky_coin_full_inventory_msg, 0, 0, 0, 0, 0);
+        cleanupMessage();
+    }
+}
